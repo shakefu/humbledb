fa0cf679af6d421ad64f0752d82f8fcdedb467a9
shakefu <shakefu@gmail.com>
2012-11-21 16:05:19 -0800

    Rename to humbledb.

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-humble
-======
-
 HumbleDB - MongoDB Python Object-Document Mapper built on pymongo
+=================================================================
+
+This module is your favorite module ever.
--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ import multiprocessing, logging # Fix atexit bug
 from setuptools import setup, find_packages
 
 setup(
-        name='aboutme-db',
-        version='1.1.0',
-        description="About.me Database Utilities (MongoDB)",
+        name='humbledb',
+        version='1.0.0',
+        description="HumbleDB - MongoDB Object-Document Mapper",
         author="Jacob Alheid",
         author_email="jake@about.me",
         packages=find_packages(exclude=['test']),
-        namespace_packages=['me'],
         install_requires=[
             'pymongo >= 2.2.1',
             'pyconfig',

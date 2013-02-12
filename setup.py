2b7ebc23a63d348663c1430e63b0f117052a725a
shakefu <shakefu@gmail.com>
2013-02-11 22:34:38 -0800

    Depend on pytool >= 2.2.0.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         install_requires=[
             'pymongo >= 2.0.1',
             'pyconfig',
-            'pytool >= 2.0.0',
+            'pytool >= 2.2.0',
             ],
         test_suite='nose.collector',
         tests_require=[

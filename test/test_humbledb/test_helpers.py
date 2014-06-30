fea64106c277a52e364f624d2db0d9e61ed39005
shakefu <shakefu@gmail.com>
2014-06-30 14:05:42 -0700

    Don't test DB with old versions.

--- test/test_humbledb/test_helpers.py
+++ test/test_humbledb/test_helpers.py
@@ -5,10 +5,11 @@ Tests for Helpers
 """
 import pyconfig
 
+from humbledb import _version
 from humbledb import Document, Mongo
 from humbledb.helpers import auto_increment
-from ..util import DBTest, database_name, eq_, raises
 from humbledb.errors import DatabaseMismatch, NoConnection
+from ..util import DBTest, database_name, eq_, raises, SkipTest
 
 
 SIDECAR = 'sidecars'
@@ -42,6 +43,9 @@ def test_auto_increment_works_as_advertised():
 
 @raises(DatabaseMismatch)
 def test_auto_increment_errors_with_wrong_db():
+    if _version._lt('2.6.0'):
+        raise SkipTest
+
     host = pyconfig.get('humbledb.test.db.host', 'localhost')
     port = pyconfig.get('humbledb.test.db.port', 27017)
     uri = 'mongodb://{}:{}/{}'.format(host, port, database_name())
